e3078f3194bf6c5cc922e8b92ffe9c63320ec2af
joao.delgado <joao.delgado@vilt-group.com>
2026-04-27 08:12:34 -0300

    fix: replace logo with move_logo.png

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -8,7 +8,7 @@ interface LogoProps {
 export default function Logo({ height = 40, className = "" }: LogoProps) {
   return (
     <Image
-      src="/images/Academia-move.svg"
+      src="/images/move_logo.png"
       alt="Move Academia"
       height={height}
       width={Math.round(height * 3.4)}
